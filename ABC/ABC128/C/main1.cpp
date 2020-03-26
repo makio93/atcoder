ee03f486519a87af8ac1ca253b4be45d42561e0a
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-03-27 07:19:11 +0900

    全探索abc128c完了

---
+++ ABC/ABC128/C/main1.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 総数を1000000007（素数）で割った余り
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+const long long MOD = 2;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vi k(m), s(n, 0);
+    rep(i, m) {
+        cin >> k[i];
+        rep(j, k[i]) {
+            int si;
+            cin >> si;
+            --si;
+            s[si] |= 1<<i;
+        }
+    }
+    int p = 0;
+    rep(i, m) {
+        int pi;
+        cin >> pi;
+        p |= pi<<i;
+    }
+    int ans = 0;
+    rep(i, 1<<n) {
+        int t = 0;
+        rep(j, n) {
+            if (i>>j&1) t ^= s[j];
+        }
+        if (t == p) ++ans;
+    }
+    cout << ans << endl;
+    return 0;
+}
