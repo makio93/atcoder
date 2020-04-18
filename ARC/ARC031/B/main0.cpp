c5a1228f99f768b137d165def0cbe6cd87c211ba
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-04-18 21:39:25 +0900

    蟻本LakeCounting類題arc031b完了

---
+++ ARC/ARC031/B/main0.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 総数を1000000007（素数）で割った余り
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+const string YES = "YES";
+const string NO = "NO";
+
+vs a(10), b(10);
+void dfs(int i, int j) {
+    if (i<0||i>=10||j<0||j>=10) return;
+    if (b[i][j]!='o') return;
+    b[i][j] = ' ';
+    dfs(i+1, j);
+    dfs(i-1, j);
+    dfs(i, j+1);
+    dfs(i, j-1);
+}
+
+int main(){
+    rep(i, 10) cin >> a[i];
+    bool ans = false;
+    rep(i, 10) rep(j, 10) {
+        b = a;
+        b[i][j] = 'o';
+        dfs(i, j);
+        bool res = true;
+        rep(i2, 10) rep(j2, 10) {
+            if (b[i2][j2]=='o') res = false;
+        }
+        if (res) ans = true;
+    }
+    cout << (ans?YES:NO) << endl;
+    return 0;
+}
