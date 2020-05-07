05c00030113a642fba6d508e4e6ba6d17e0305d8
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-05-07 12:31:43 +0900

    参加済abc162動画解説の復習完了およびスニペットprime作成

---
+++ ABC/ABC162/E/main3.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using P = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+// Sieve of Eratosthenes
+struct Sieve {
+    int n;
+    vector<int> f, primes;
+    Sieve(int n=1):n(n), f(n+1) {
+        f[0] = f[1] = -1;
+        for (ll i = 2; i <= n; ++i) {
+            if (f[i]) continue;
+            primes.push_back(i);
+            f[i] = i;
+            for (ll j = i*i; j <= n; j += i) {
+                if (!f[j]) f[j] = i;
+            }
+        }
+    }
+    bool isPrime(int x) { return f[x] == x;}
+    vector<int> factorList(int x) {
+        vector<int> res;
+        while (x != 1) {
+            res.push_back(f[x]);
+            x /= f[x];
+        }
+        return res;
+    }
+    vector<P> factor(int x) {
+        vector<int> fl = factorList(x);
+        if (fl.size() == 0) return {};
+        vector<P> res(1, P(fl[0], 0));
+        for (int p : fl) {
+            if (res.back().first == p) {
+                res.back().second++;
+            } else {
+                res.emplace_back(p, 1);
+            }
+        }
+        return res;
+    }
+};
+
+// auto mod int
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vector<mint> d(k+1);
+    rep1(i, k) d[i] = mint(k/i).pow(n);
+    Sieve s(k);
+    for (int p : s.primes) {
+        for (ll i=p; i<=k; i+=p) {
+            d[i/p] -= d[i];
+        }
+    }
+    mint ans = 0;
+    rep1(i, k) ans += d[i] * i;
+    cout << ans << endl;
+    return 0;
+}
