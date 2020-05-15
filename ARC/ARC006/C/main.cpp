0e1f1d43f3a3163f7a85cc7f3f0b14c0c4f8429e
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-05-15 16:29:36 +0900

    蟻本「Saruman's Army」類題arc006c演習完了

---
+++ ARC/ARC006/C/main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 総数を1000000007（素数）で割った余り
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+int main(){
+    int n;
+    cin >> n;
+    vi w(n);
+    rep(i, n) cin >> w[i];
+    vi a;
+    rep(i, n) {
+        int dist = INF, itr = -1;
+        rep(j, sz(a)) {
+            if (w[i] > a[j]) continue;
+            if (dist > a[j] - w[i]) {
+                dist = a[j] - w[i];
+                itr = j;
+            }
+        }
+        if (itr == -1) a.pb(w[i]);
+        else a[itr] = w[i];
+    }
+    cout << sz(a) << endl;
+    return 0;
+}
