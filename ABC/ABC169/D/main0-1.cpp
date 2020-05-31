9cacd68f664549f20d9da307b33e54b3eca3e7be
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-05-31 22:49:53 +0900

    本番abc169参加後

---
+++ ABC/ABC169/D/main0-1.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 総数を1000000007（素数）で割った余り
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+using P = pii;
+
+// Sieve of Eratosthenes
+struct Sieve {
+    int n;
+    vector<int> f, primes;
+    Sieve(int n=1):n(n), f(n+1) {
+        f[0] = f[1] = -1;
+        for (ll i = 2; i <= n; ++i) {
+            if (f[i]) continue;
+            primes.push_back(i);
+            f[i] = i;
+            for (ll j = i*i; j <= n; j += i) {
+                if (!f[j]) f[j] = i;
+            }
+        }
+    }
+    bool isPrime(int x) { return f[x] == x;}
+    vector<int> factorList(int x) {
+        vector<int> res;
+        while (x != 1) {
+            res.push_back(f[x]);
+            x /= f[x];
+        }
+        return res;
+    }
+    vector<P> factor(int x) {
+        vector<int> fl = factorList(x);
+        if (fl.size() == 0) return {};
+        vector<P> res(1, P(fl[0], 0));
+        for (int p : fl) {
+            if (res.back().first == p) {
+                res.back().second++;
+            } else {
+                res.emplace_back(p, 1);
+            }
+        }
+        return res;
+    }
+};
+
+int main(){
+    ll n;
+    cin >> n;
+    vector<bool> p(1001, false);
+    for (int i=2; i<=sqrt(1000); ++i) {
+        if (p[i]) continue;
+        for (int j=2; i*j<=1000; ++j) {
+            p[i*j] = true;
+        }
+    }
+    priority_queue<int, vi, greater<int>> q;
+    int ans = 0;
+    for (int i=2; i<sz(p); ++i) {
+        if (!p[i]) q.emplace(i);
+    }
+    while (n>1) {
+        ll x = q.top(); q.pop();
+        if (n<x) break;
+        if (n%x != 0) continue;
+        n /= x;
+        ++ans;
+        q.emplace(x*x);
+    }
+    cout << ans << endl;
+    return 0;
+}
