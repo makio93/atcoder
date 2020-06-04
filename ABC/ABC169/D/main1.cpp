abf1b2b37e1488f10ecf72d81dadcae928fc1c59
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-06-04 18:22:08 +0900

    本番参加済abc169復習完了およびスニペットprimeの内容を更新

---
+++ ABC/ABC169/D/main1.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 総数を1000000007（素数）で割った余り
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+int main(){
+    ll n;
+    cin >> n;
+    vector<pair<ll, int>> fact;
+    for (ll i=2; i*i<=n; ++i) {
+        int cnt = 0;
+        while (n%i==0) {
+            n /= i;
+            ++cnt;
+        }
+        if (cnt > 0) fact.emplace_back(i, cnt);
+    }
+    if (n > 1) fact.emplace_back(n, 1);
+    int ans = 0;
+    for (auto p : fact) {
+        int x = 1;
+        while (x <= p.second) {
+            p.second -= x;
+            ++x;
+            ++ans;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
---
+++ ABC/ABC169/D/main2.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 総数を1000000007（素数）で割った余り
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+using P = pair<ll, int>;
+
+// Sieve of Eratosthenes
+struct Sieve {
+    int n;
+    vector<int> f, primes;
+    Sieve(int n=1):n(n), f(n+1) {
+        f[0] = f[1] = -1;
+        for (ll i = 2; i <= n; ++i) {
+            if (f[i]) continue;
+            primes.push_back(i);
+            f[i] = i;
+            for (ll j = i*i; j <= n; j += i) {
+                if (!f[j]) f[j] = i;
+            }
+        }
+    }
+    bool isPrime(int x) { return f[x] == x;}
+    vector<ll> factorList(ll x) {
+        vector<ll> res;
+        for (int p : primes) {
+            while (x%p==0) {
+                x /= p;
+                res.pb((ll)(p));
+            }
+        }
+        if (x > 1) res.pb((ll)(x));
+        return res;
+    }
+    vector<P> factor(ll x) {
+        vector<ll> fl = factorList(x);
+        if (fl.size() == 0) return {};
+        vector<P> res(1, P(fl[0], 0));
+        for (ll p : fl) {
+            if (res.back().first == p) {
+                res.back().second++;
+            } else {
+                res.emplace_back(p, 1);
+            }
+        }
+        return res;
+    }
+};
+
+int main(){
+    ll n;
+    cin >> n;
+    Sieve s((int)(1e6+5));
+    vector<P> vp = s.factor(n);
+    int ans = 0;
+    for (auto p : vp) {
+        int x = 1;
+        while (p.second-x >= 0) {
+            p.second -= x;
+            ++x;
+            ++ans;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
