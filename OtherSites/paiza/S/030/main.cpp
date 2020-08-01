dd9343279ee7b80c224c75d892da3100af48c789
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-08-01 21:33:41 +0900

    paizaスキルチェック解答作成

---
+++ OtherSites/paiza/S/030/main.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+struct UnionFind {
+  vector<int> d;
+  UnionFind(int n=0): d(n,-1) {}
+  int find(int x) {
+    if (d[x] < 0) return x;
+    return d[x] = find(d[x]);
+  }
+  bool unite(int x, int y) {
+    x = find(x); y = find(y);
+    if (x == y) return false;
+    if (d[x] > d[y]) swap(x,y);
+    d[x] += d[y];
+    d[y] = x;
+    return true;
+  }
+  bool same(int x, int y) { return find(x) == find(y);}
+  int size(int x) { return -d[find(x)];}
+};
+
+int main(){
+    int n;
+    cin >> n;
+    vector<vi> g(n);
+    UnionFind uf(n);
+    vector<bool> r(n);
+    rep1(i, n-1) {
+        int a;
+        cin >> a;
+        --a;
+        g[i].pb(a);
+        uf.unite(i, a);
+        r[a] = true;
+    }
+    map<int, bool> mp;
+    rep(i, n) {
+        mp[uf.find(i)] = true;
+    }
+    rep(i, n) if (!r[i]) {
+        mp[uf.find(i)] = false;
+    }
+    int ans = 0;
+    ans += count(all(r), false);
+    for (auto p : mp) {
+        if (p.second) ++ans;
+    }
+    cout << (ans-1) << endl;
+    return 0;
+}
