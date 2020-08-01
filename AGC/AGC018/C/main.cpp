c70fe0854d2526f330ed8bd3a651f8cabc2e94d6
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-08-02 01:02:11 +0900

    AtCoder ProblemsのRecomendationからagc018aを練習

---
+++ AGC/AGC018/C/main.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+
+void func(long long X, long long Y, long long Z, std::vector<long long> A, std::vector<long long> B, std::vector<long long> C){
+
+}
+
+int main(){
+    // cout << fixed << setprecision(5);
+
+    long long X;
+    scanf("%lld",&X);
+    long long Y;
+    scanf("%lld",&Y);
+    long long Z;
+    scanf("%lld",&Z);
+    std::vector<long long> A(X+Y+Z);
+    std::vector<long long> B(X+Y+Z);
+    std::vector<long long> C(X+Y+Z);
+    for(int i = 0 ; i < X+Y+Z ; i++){
+        scanf("%lld",&A[i]);
+        scanf("%lld",&B[i]);
+        scanf("%lld",&C[i]);
+    }
+    func(X, Y, Z, std::move(A), std::move(B), std::move(C));
+    return 0;
+}
