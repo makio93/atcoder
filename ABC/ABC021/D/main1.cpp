7da33588d98ca506e7f03a2f235a97ec832d86e1
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-08-23 23:11:50 +0900

    蟻本類題2-3-8-2問目abc021d学習完了

---
+++ ABC/ABC021/D/main1.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+const long long MOD = 1000000007;
+
+ll modPow(int a, int b) {
+    if (b == 0) return 1;
+    if (b%2==0) {
+        ll half = modPow(a, b/2);
+        return half * half % MOD;
+    }
+    else {
+        return a * modPow(a, b-1) % MOD;
+    }
+}
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vll fact(k+n), inv(k+n);
+    fact[0] = 1;
+    rep1(i, k+n-1) fact[i] = fact[i-1] * i % MOD;
+    inv[k+n-1] = modPow(fact[k+n-1], MOD-2);
+    repr(i, k+n-1) inv[i] = inv[i+1] * (i+1) % MOD;
+    ll ans = fact[k+n-1] * inv[n-1] % MOD * inv[k] % MOD;
+    cout << ans << endl;
+    return 0;
+}
