532ba26270033930a2676d89ba1090eeec84feb8
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-09-23 00:25:58 +0900

    緑diff埋め9/22-1:abc138d自力AC完了

--- ABC/ABC138/D/main-temp.cpp
+++ ABC/ABC138/D/main-temp.cpp
@@ -35,36 +35,30 @@ using pll = pair<ll, ll>;
 ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
 ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
 
-vector<vi> g;
-vi x, val;
-void dfs(int v, int p=-1, int c=0) {
-    val[v] = c + x[v];
-    for (int t : g[v]) {
-        if (t == p) continue;
-        dfs(t, v, val[v]);
-    }
+
+void func(long long N, long long Q, std::vector<long long> a, std::vector<long long> b, std::vector<long long> p, std::vector<long long> x){
+
 }
 
 int main(){
-    int n, q;
-    cin >> n >> q;
-    g.resize(n);
-    val.resize(n);
-    x.resize(n);
-    rep(i, n-1) {
-        int a, b;
-        cin >> a >> b;
-        --a; --b;
-        g[a].pb(b);
-        g[b].pb(a);
+    // cout << fixed << setprecision(5);
+
+    long long N;
+    scanf("%lld",&N);
+    long long Q;
+    scanf("%lld",&Q);
+    std::vector<long long> a(N-1);
+    std::vector<long long> b(N-1);
+    for(int i = 0 ; i < N-1 ; i++){
+        scanf("%lld",&a[i]);
+        scanf("%lld",&b[i]);
     }
-    rep(i, q) {
-        int p, xi;
-        cin >> p >> xi;
-        --p;
-        x[p] += xi;
+    std::vector<long long> p(Q);
+    std::vector<long long> x(Q);
+    for(int i = 0 ; i < Q ; i++){
+        scanf("%lld",&p[i]);
+        scanf("%lld",&x[i]);
     }
-    dfs(0);
-    rep(i, n) printf("%d%c", val[i], (i<n-1?' ':'\n'));
+    func(N, Q, std::move(a), std::move(b), std::move(p), std::move(x));
     return 0;
 }
