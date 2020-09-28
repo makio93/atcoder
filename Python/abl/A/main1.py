cb97282fb2333cab42cfcde92c8a677d88a51fc8
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-09-29 00:10:24 +0900

    本番参加後abl復習Pythonコード版a-b完了

---
+++ Python/abl/A/main1.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python3
+
+k = int(input())
+print('ACL'*k)
---
+++ Python/abl/B/main1.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+
+a,b,c,d = map(int, input().split())
+if b < c or d < a:
+    print('No')
+else:
+    print('Yes')
