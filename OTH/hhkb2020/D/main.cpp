181bb9cf7a5c149d9b285ce8fffe896d9f7aef1d
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-10-10 23:13:27 +0900

    コンテストhhkb2020本番参加後

---
+++ OTH/hhkb2020/D/main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define snuke(c, itr) for (__typeof((c).begin()) itr = (c).begin(); itr != (c).end(); itr++)
+#define snuker(c, itr) for (__typeof((c).rbegin()) itr = (c).rbegin(); itr != (c).rend(); itr++)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define INF2 (1e18)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+using mint = modint1000000007;
+
+int main(){
+    int t;
+    cin >> t;
+    rep(i, t) {
+        ll n, a, b;
+        cin >> n >> a >> b;
+        if (a > b) swap(a, b);
+        mint anum = mint(n-a+1).pow(2), bnum = mint(n-b+1).pow(2);
+        mint snum = bnum * mint(b-1).pow(2);
+        //ll a2 = min(a, n-a-b+2);
+        mint ssnum = 0;// (mint(a2) * (a2-1) / 2) * (n-a2-b+1) * 4;
+        mint ans = anum * bnum - snum + ssnum;
+        cout << (ans / 8 * 5).val() << endl;
+    }
+    return 0;
+}
