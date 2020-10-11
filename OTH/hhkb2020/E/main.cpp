6327b7bbf06bb3fd4ac1980861853c3dd401cd01
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-10-11 11:07:08 +0900

    hhkb2020本番参加後e問題自力研究

--- OTH/hhkb2020/E/main.cpp
+++ OTH/hhkb2020/E/main.cpp
@@ -41,8 +41,107 @@ using pll = pair<ll, ll>;
 ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
 ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
 
-const long long mod = 1000000007;
+using mint = modint1000000007;
 
 int main(){
+    int h, w;
+    cin >> h >> w;
+    vs s(h);
+    rep(i, h) cin >> s[i];
+    int k = 0;
+    rep(i, h) rep(j, w) {
+        if (s[i][j] == '.') ++k;
+    }
+    vector<vi> val(h, vi(w));
+    rep(i, h) {
+        int now = 0, l = 0;
+        rep(j, w) {
+            if (s[i][j] == '#') {
+                if (now > 0) {
+                    for (int j2=l; j2<j; ++j2) {
+                        val[i][j2] += now;
+                    }
+                    now = 0;
+                }
+                l = j+1;
+            }
+            else ++now;
+        }
+        if (now > 0) {
+            for (int j2=l; j2<w; ++j2) {
+                val[i][j2] += now;
+            }
+        }
+    }
+    rep(i, w) {
+        int now = 0, u = 0;
+        rep(j, h) {
+            if (s[j][i] == '#') {
+                if (now > 0) {
+                    for (int j2=u; j2<j; ++j2) {
+                        val[j2][i] += now;
+                    }
+                    now = 0;
+                }
+                u = j+1;
+            }
+            else ++now;
+        }
+        if (now > 0) {
+            for (int j2=u; j2<h; ++j2) {
+                val[j2][i] += now;
+            }
+        }
+    }
+    rep(i, h) rep(j, w) {
+        val[i][j] = max(0, val[i][j]-1);
+    }
+    map<pii, int> cnt;
+    rep(i, h) {
+        int now = INF, l = 0;
+        rep(j, w) {
+            if (val[i][j] == 0) {
+                if (l < j) {
+                    pii res = { now, (j-l) };
+                    cnt[res]++;
+                    now = INF;
+                }
+                l = j+1;
+            }
+            else now = min(now, val[i][j]);
+        }
+        if (l < w) {
+            pii res = { now, (w-l) };
+            cnt[res]++;
+        }
+    }
+    rep(i, w) {
+        int now = INF, u = 0;
+        rep(j, h) {
+            if (val[j][i] == 0) {
+                if (u < j) {
+                    pii res = { now, (j-u) };
+                    cnt[res]++;
+                    now = INF;
+                }
+                u = j+1;
+            }
+            else now = min(now, val[j][i]);
+        }
+        if (u < h) {
+            pii res = { now, (h-u) };
+            cnt[res]++;
+        }
+    }
+    mint sub = 0;
+    for (auto p : cnt) {
+        int fif = p.first.first, fis = p.first.second, se = p.second;
+        if (fis <= 1) continue;
+        sub += mint(2).pow(k-fif) * (mint(2).pow(fif-1) - 1) * fis * se;
+    }
+    ll vsum = 0;
+    rep(i, h) rep(j, w) vsum += val[i][j];
+    mint ans = mint(2).pow(k-1) * vsum - sub;
+    cout << ans.val() << endl;
     return 0;
 }
