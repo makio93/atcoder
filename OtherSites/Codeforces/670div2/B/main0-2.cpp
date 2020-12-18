cbeb3abe824b3eeba491ab94a5502d8d38c30829
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-12-18 15:54:29 +0900

    本番参加後のこどふぉ670div2-bをあらためて自力ACした

--- OtherSites/Codeforces/670div2/B/main0-2.cpp
+++ OtherSites/Codeforces/670div2/B/main0-2.cpp
@@ -39,7 +39,45 @@ using pll = pair<ll, ll>;
 ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
 ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
 
+// 本番終了後での自力提出コード
 
 int main(){
+    int t;
+    cin >> t;
+    rep(i1, t) {
+        int n;
+        cin >> n;
+        vi a(n);
+        rep(i, n) cin >> a[i];
+        if (n <= 5) {
+            ll ans = 1;
+            rep(i, n) ans *= a[i];
+            cout << ans << endl;
+            continue;
+        }
+        vi plus, minus;
+        rep(i, n) {
+            if (a[i] >= 0) plus.pb(a[i]);
+            else minus.pb(a[i]);
+        }
+        ll ans = 1;
+        if (plus.empty()) {
+            RSORT(minus);
+            rep(i, 5) ans *= minus[i];
+        }
+        else {
+            RSORT(plus);
+            VSORT(minus);
+            ans = 0;
+            for (int i=0; i<=min(5,sz(minus)); i+=2) {
+                if (sz(plus) < 5-i) continue;
+                ll tval = 1;
+                rep(j, i) tval *= minus[j];
+                rep(j, 5-i) tval *= plus[j];
+                ans = max(ans, tval);
+            }
+        }
+        cout << ans << endl;
+    }
     return 0;
 }
