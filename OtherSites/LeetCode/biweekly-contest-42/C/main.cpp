958a825a996934c62acdc94cda9347cafcd8417c
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2020-12-27 02:07:04 +0900

    biweekly-contest-42本番参加

---
+++ OtherSites/LeetCode/biweekly-contest-42/C/main.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+using namespace std;
+//using namespace atcoder;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define snuke(c, itr) for (__typeof((c).begin()) itr = (c).begin(); itr != (c).end(); itr++)
+#define snuker(c, itr) for (__typeof((c).rbegin()) itr = (c).rbegin(); itr != (c).rend(); itr++)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+//#define mp make_pair
+
+#define INF (1e9)
+#define INF2 (1e18)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+class Solution {
+public:
+    string maximumBinaryString(string binary) {
+        int n = binary.size();
+        if (binary == string(n, '1')) return binary;
+        int llen = 0, bcnt = 0, rlen = 0;
+        for (char c : binary) {
+            if (c == '0') ++llen;
+            else break;
+        }
+        string res = string(n, '0');
+        if (llen > 0) {
+            for (int i=0; i<llen-1; ++i) {
+                res[i] = '1';
+            }
+            for (int i=llen; i<n; ++i) {
+                if (binary[i] == '1') ++bcnt;
+            }
+            rlen = (n-llen) - bcnt;
+            int mstart = max(0, llen-1), mlen = (llen+rlen-1) - mstart;
+            for (int i=0; i<mlen; ++i) {
+                res[mstart+i] = '1';
+            }
+        }
+        else {
+            int llen2 = 0;
+            for (char c : binary) {
+                if (c == '1') ++llen2;
+                else break;
+            }
+            for (int i=llen2; i<n; ++i) {
+                if (binary[i] == '1') ++bcnt;
+            }
+            for (int i=0; i<(n-bcnt-1); ++i) res[i] = '1';
+        }
+        for (int i=0; i<bcnt; ++i) {
+            res[n-i-1] = '1';
+        }
+        return res;
+    }
+};
+
+int main(){
+    Solution cl;
+    string binary;
+    cin >> binary;
+    string res = cl.maximumBinaryString(binary);
+    cout << res << endl;
+    return 0;
+}
