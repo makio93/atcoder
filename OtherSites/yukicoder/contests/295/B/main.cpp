c172b59470ac3f3a03058ad094437029bcab08b5
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-01-15 23:49:36 +0900

    yukicodercontest278本番参加

---
+++ OtherSites/yukicoder/contests/295/B/main.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+#define v(t) vector<t>
+#define p(t) pair<t, t>
+#define p2(t, s) pair<t, s>
+#define vp(t) v(p(t))
+
+#define rep(i, n) for (int i=0,i##_len=((int)(n)); i<i##_len; ++i)
+#define rep2(i, a, n) for (int i=((int)(a)),i##_len=((int)(n)); i<=i##_len; ++i)
+#define repr(i, n) for (int i=((int)(n)-1); i>=0; --i)
+#define rep2r(i, a, n) for (int i=((int)(n)),i##_len=((int)(a)); i>=i##_len; --i)
+
+#define repi(itr, c) for (__typeof((c).begin()) itr=(c).begin(); itr!=(c).end(); ++itr)
+#define repir(itr, c) for (__typeof((c).rbegin()) itr=(c).rbegin(); itr!=(c).rend(); ++itr)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define eb emplace_back
+
+#define INF (1e9)
+#define LINF (1e18)
+#define PI (acos(-1))
+#define EPS (1e-7)
+#define DEPS (1e-10)
+
+const ll mod = (ll)(INF);
+
+// auto mod int
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+};
+
+int main(){
+    int t;
+    cin >> t;
+    rep(i1, t) {
+        int n;
+        cin >> n;
+        v(int) a(n);
+        rep(i, n) cin >> a[i];
+        v(mint) sum(n+1);
+        rep(i, n) {
+            sum[i+1] = (sum[i] + a[i]);
+            sum[i+1] += (sum[i] * a[i]);
+        }
+        cout << sum[n].x << endl;
+    }
+    return 0;
+}
