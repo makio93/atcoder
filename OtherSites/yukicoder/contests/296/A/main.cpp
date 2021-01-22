d8cf50360105422f400a98927afe815bb7af9553
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-01-22 23:52:41 +0900

    yukicoder-contest279本番参加後

---
+++ OtherSites/yukicoder/contests/296/A/main.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+#define v(t) vector<t>
+#define p(t) pair<t, t>
+#define p2(t, s) pair<t, s>
+#define vp(t) v(p(t))
+
+#define rep(i, n) for (int i=0,i##_len=((int)(n)); i<i##_len; ++i)
+#define rep2(i, a, n) for (int i=((int)(a)),i##_len=((int)(n)); i<=i##_len; ++i)
+#define repr(i, n) for (int i=((int)(n)-1); i>=0; --i)
+#define rep2r(i, a, n) for (int i=((int)(n)),i##_len=((int)(a)); i>=i##_len; --i)
+
+#define repi(itr, c) for (__typeof((c).begin()) itr=(c).begin(); itr!=(c).end(); ++itr)
+#define repir(itr, c) for (__typeof((c).rbegin()) itr=(c).rbegin(); itr!=(c).rend(); ++itr)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define eb emplace_back
+
+#define INF (1e9)
+#define LINF (1e18)
+#define PI (acos(-1))
+#define EPS (1e-7)
+#define DEPS (1e-10)
+
+const ll mod2 = (ll)(1e9+7);
+ll mod = (ll)(1e9+7);
+
+// auto mod int
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
+int main(){
+    int t;
+    cin >> t;
+    rep(i1, t) {
+        v(int) n(3);
+        ll y;
+        rep(i, 3) cin >> n[i];
+        cin >> y;
+        RSORT(n);
+        v(ll) vals;
+        ll nval = 0;
+        while (nval <= y) {
+            vals.pb(y-nval);
+            nval += n[0];
+        }
+        ll ans = 0, nlcm = lcm((ll)(n[1]), (ll)(n[2]));
+        ll gval = gcd((ll)(n[1]), (ll)(n[2]));
+        ll n1d = n[1] / gval, n2d = n[2] / gval;
+        mod = n1d;
+        rep(i, sz(vals)) {
+            mint cval = n[1];
+            if (mod > 1) cval /= n[2];
+            if (vals[i]-cval.x*n[2] < 0) continue;
+            ans = (ans + (vals[i]-cval.x*n[2]) / nlcm + 1) % mod2;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
---
+++ OtherSites/yukicoder/contests/296/B/main.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+#define v(t) vector<t>
+//#define p(t) pair<t, t>
+//#define p2(t, s) pair<t, s>
+#define vp(t) v(p(t))
+
+#define rep(i, n) for (int i=0,i##_len=((int)(n)); i<i##_len; ++i)
+#define rep2(i, a, n) for (int i=((int)(a)),i##_len=((int)(n)); i<=i##_len; ++i)
+#define repr(i, n) for (int i=((int)(n)-1); i>=0; --i)
+#define rep2r(i, a, n) for (int i=((int)(n)),i##_len=((int)(a)); i>=i##_len; --i)
+
+#define repi(itr, c) for (__typeof((c).begin()) itr=(c).begin(); itr!=(c).end(); ++itr)
+#define repir(itr, c) for (__typeof((c).rbegin()) itr=(c).rbegin(); itr!=(c).rend(); ++itr)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define eb emplace_back
+
+#define INF (1e9)
+#define LINF (1e18)
+#define PI (acos(-1))
+#define EPS (1e-7)
+#define DEPS (1e-10)
+
+ll mod = (1e9+7);
+
+struct mint {
+    ll x;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
+int main(){
+    int n, k, m;
+    cin >> n >> k >> m;
+    mod = m;
+    v(v(int)) p(4, v(int)(n));
+    rep(i1, 4) rep(i, n) cin >> p[i1][i];
+    rep(i, 4) { VSORT(p[i]); }
+    mint sum(0);
+    rep(i, n) {
+        int mnval = (1e9+5), mxval = 0;
+        rep(i1, 4) {
+            mnval = min(mnval, p[i1][i]);
+            mxval = max(mxval, p[i1][i]);
+        }
+        sum += mint(mxval-mnval).pow(k);
+    }
+    cout << sum << endl;
+    return 0;
+}
