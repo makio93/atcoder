2f050961cff36f17beefaae717d4831eab9a1b91
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-01-23 17:44:32 +0900

    abc170-eを気まぐれ精進、自力AC完了

--- ABC/ABC170/E/main.cpp
+++ ABC/ABC170/E/main.cpp
@@ -1,68 +1,76 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-
-// 総数を1000000007（素数）で割った余り
-const long long mod = 1e9 + 7;
+using namespace atcoder;
 
 using ll = long long;
-using pii  = pair<int, int>;
-using pll = pair<ll, ll>;
-#define ull unsigned long long
-#define ld long double
-#define vi vector<int>
-#define vll vector<ll>
-#define vc vector<char>
-#define vs vector<string>
-#define vpii vector<pii>
-#define vpll vector<pll>
+using ull = unsigned long long;
+#define v(t) vector<t>
+#define p(t) pair<t, t>
+#define p2(t, s) pair<t, s>
+#define vp(t) v(p(t))
 
-#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
-#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
-#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
-#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+#define rep(i, n) for (int i=0,i##_len=((int)(n)); i<i##_len; ++i)
+#define rep2(i, a, n) for (int i=((int)(a)),i##_len=((int)(n)); i<=i##_len; ++i)
+#define repr(i, n) for (int i=((int)(n)-1); i>=0; --i)
+#define rep2r(i, a, n) for (int i=((int)(n)),i##_len=((int)(a)); i>=i##_len; --i)
+
+#define repi(itr, c) for (__typeof((c).begin()) itr=(c).begin(); itr!=(c).end(); ++itr)
+#define repir(itr, c) for (__typeof((c).rbegin()) itr=(c).rbegin(); itr!=(c).rend(); ++itr)
 
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 
-#define SORT(v, n) sort(v, v + n);
+#define SORT(v, n) sort(v, v+n);
 #define VSORT(v) sort(v.begin(), v.end());
 #define RSORT(x) sort(rall(x));
 #define pb push_back
-#define mp make_pair
+#define eb emplace_back
 
 #define INF (1e9)
+#define LINF (1e18)
 #define PI (acos(-1))
 #define EPS (1e-7)
+#define DEPS (1e-10)
 
-ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
-ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
-
-
-void func(long long N, long long Q, std::vector<long long> A, std::vector<long long> B, std::vector<long long> C, std::vector<long long> D){
-
-}
+// きまぐれ精進、自力AC
 
 int main(){
-    // cout << fixed << setprecision(5);
-
-    long long N;
-    scanf("%lld",&N);
-    long long Q;
-    scanf("%lld",&Q);
-    std::vector<long long> A(N);
-    std::vector<long long> B(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&A[i]);
-        scanf("%lld",&B[i]);
+    int n, q;
+    cin >> n >> q;
+    v(int) a(n), b(n);
+    rep(i, n) cin >> a[i] >> b[i];
+    v(int) garden(n);
+    rep(i, n) garden[i] = b[i];
+    map<int, set<p(int)>> infant;
+    rep(i, n) infant[b[i]].emplace(a[i], i);
+    set<p(int)> leader;
+    repi(itr, infant) leader.emplace(itr->second.rbegin()->first, itr->first);
+    rep(i, q) {
+        int c, d;
+        cin >> c >> d;
+        --c;
+        int gi = garden[c], ai = a[c];
+        garden[c] = d;
+        if (infant[gi].rbegin()->second == c) {
+            infant[gi].erase({ai,c});
+            leader.erase({ai,gi});
+            if (!infant[gi].empty()) leader.emplace(infant[gi].rbegin()->first, gi);
+        }
+        else infant[gi].erase({ai,c});
+        if (!infant[d].empty() && *(infant[d].rbegin()) < make_pair(ai,c)) {
+            auto plead = make_pair(infant[d].rbegin()->first, d);
+            infant[d].emplace(ai,c);
+            leader.erase(plead);
+            leader.emplace(ai,d);
+        }
+        else if (infant[d].empty()) {
+            infant[d].emplace(ai,c);
+            leader.emplace(ai,d);
+        }
+        else infant[d].emplace(ai,c);
+        cout << (leader.begin()->first) << endl;
     }
-    std::vector<long long> C(Q);
-    std::vector<long long> D(Q);
-    for(int i = 0 ; i < Q ; i++){
-        scanf("%lld",&C[i]);
-        scanf("%lld",&D[i]);
-    }
-    func(N, Q, std::move(A), std::move(B), std::move(C), std::move(D));
     return 0;
 }
-
