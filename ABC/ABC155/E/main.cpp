2b8cc564402d018cb578a12c6246f682e20514bb
Ken Makino <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-02-01 21:29:10 +0900

    abc155e問題、きまぐれ精進、自力AC

--- ABC/ABC155/E/main.cpp
+++ ABC/ABC155/E/main.cpp
@@ -1,54 +1,50 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-
-// 総数を1000000007（素数）で割った余り
-const long long mod = 1e9 + 7;
+using namespace atcoder;
 
 using ll = long long;
-using pii  = pair<int, int>;
-using pll = pair<ll, ll>;
-#define ull unsigned long long
-#define ld long double
-#define vi vector<int>
-#define vll vector<ll>
-#define vc vector<char>
-#define vs vector<string>
-#define vpii vector<pii>
-#define vpll vector<pll>
+using ull = unsigned long long;
+#define v(t) vector<t>
+#define p(t) pair<t, t>
+#define p2(t, s) pair<t, s>
+#define vp(t) v(p(t))
 
-#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
-#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
-#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
-#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+#define rep(i, n) for (int i=0,i##_len=((int)(n)); i<i##_len; ++i)
+#define rep2(i, a, n) for (int i=((int)(a)),i##_len=((int)(n)); i<=i##_len; ++i)
+#define repr(i, n) for (int i=((int)(n)-1); i>=0; --i)
+#define rep2r(i, a, n) for (int i=((int)(n)),i##_len=((int)(a)); i>=i##_len; --i)
+
+#define repi(itr, c) for (__typeof((c).begin()) itr=(c).begin(); itr!=(c).end(); ++itr)
+#define repir(itr, c) for (__typeof((c).rbegin()) itr=(c).rbegin(); itr!=(c).rend(); ++itr)
 
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 
-#define SORT(v, n) sort(v, v + n);
+#define SORT(v, n) sort(v, v+n);
 #define VSORT(v) sort(v.begin(), v.end());
 #define RSORT(x) sort(rall(x));
 #define pb push_back
-#define mp make_pair
+#define eb emplace_back
 
 #define INF (1e9)
+#define LINF (1e18)
 #define PI (acos(-1))
 #define EPS (1e-7)
-
-ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
-ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
-
-
-void func(long long N){
-
-}
+#define DEPS (1e-10)
 
 int main(){
-    // cout << fixed << setprecision(5);
-
-    long long N;
-    scanf("%lld",&N);
-    func(N);
+    string n_str;
+    cin >> n_str;
+    v(int) nval(sz(n_str));
+    rep(i, sz(n_str)) nval[i] = n_str[i] - '0';
+    v(v(int)) dp(sz(nval)+1, v(int)(2));
+    dp[sz(nval)][1] = 11;
+    repr(i, sz(nval)) {
+        dp[i][0] = min(dp[i+1][0], dp[i+1][1]) + nval[i];
+        dp[i][1] = min(dp[i+1][0]+1, dp[i+1][1]-1) + (10-nval[i]);
+    }
+    cout << min(dp[0][0], dp[0][1]) << endl;
     return 0;
 }
-
